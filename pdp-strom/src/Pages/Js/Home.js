d6a4c8635e181e2409889a43b8d0cd54ab9eed45
burxonIT <116946122+burxonIT@users.noreply.github.com>
2023-06-19 17:59:12 +0300

    sasadad
    sdsadssadsad

--- pdp-strom/src/Pages/Js/Home.js
+++ pdp-strom/src/Pages/Js/Home.js
@@ -5,11 +5,25 @@ import s from  '../Css/Home.module.css'
 import {BsSearch} from 'react-icons/bs'
 import {AiFillStar} from 'react-icons/ai'
 import {AiOutlineArrowRight} from 'react-icons/ai'
+import img1 from '../img/image 3.png'
+import img2 from '../img/image.png'
+import img3 from '../img/image 2.png'
+import img4 from '../img/image 4.png'
+import img5 from '../img/image 5.png'
+import img6 from '../img/Vector (14).png'
+import img7 from '../img/Vector (15).png'
+import img8 from '../img/Vector (16).png'
+import img9 from '../img/Vector (17).png'
+import img10 from '../img/Vector (18).png'
+import img11 from '../img/Vector (19).png'
+import img12 from '../img/Vector (20).png'
+import img13 from '../img/Vector (21).png'
+
 export default class Home extends Component {
   render() {
     return (
       <div>
-        {/* <Navbar/> */}
+        <Navbar/>
             <header className={s.bosh_menu}>
                 <div className={s.dumaloq}>
                 </div>
@@ -25,7 +39,7 @@ export default class Home extends Component {
                 <div className={s.header_card}>
                     <div className={s.card1}>
                         <div className={s.as}>
-                            <h1>Marketing</h1><h2>250.000 <span>UZS</span></h2>
+                            <h1 >Marketing</h1><h2>250.000 <span>UZS</span></h2>
                         </div>
                         <h3>Raqamli marketing bo'yicha <br /> to'liq kurs <span>| 48 soat</span></h3>
                         <div className={s.yulduz}>
@@ -33,19 +47,19 @@ export default class Home extends Component {
                         </div>
                         <button className={s.onga}><AiOutlineArrowRight/></button>
                     </div>
-                    <div  className={s.card1}>
+                    <div id={s.card0}  className={s.card1}>
                         <div className={s.as}>
-                            <h1>Marketing</h1><h2>250.000 <span>UZS</span></h2>
+                            <h1 className={s.hh2}>Dasturlash</h1><h2>250.000 <span>UZS</span></h2>
                         </div>
-                        <h3>Raqamli marketing bo'yicha <br /> to'liq kurs <span>| 48 soat</span></h3>
+                        <h3>Java Spring bo’yicha to’liqkurs(amaliy) <br /> to'liq kurs <span>| 48 soat</span></h3>
                         <div className={s.yulduz}>
-                        <AiFillStar className={s.yulduzcha}/><AiFillStar className={s.yulduzcha}/> <AiFillStar className={s.yulduzcha}/> <AiFillStar className={s.yulduzcha}/><AiFillStar className={s.yulduz1}/><p>4.1</p>
+                        <AiFillStar className={s.yulduzcha}/><AiFillStar className={s.yulduzcha}/> <AiFillStar className={s.yulduzcha}/> <AiFillStar className={s.yulduzcha}/><AiFillStar className={s.yulduz1}/><p>4.0</p>
                         </div>
                         <button className={s.onga}><AiOutlineArrowRight/></button>
                     </div>
                     <div id={s.card1} className={s.card1}>
                         <div className={s.as}>
-                            <h1>Marketing</h1><h2>400.000 <span>UZS</span></h2>
+                            <h1 className={s.hh1}>Dizayn</h1><h2>400.000 <span>UZS</span></h2>
                         </div>  
                         <h3>Illustrator 0 dan professionalgacha <br /> to'liq kurs <span>| 48 soat</span></h3>
                         <div className={s.yulduz}>
@@ -53,11 +67,11 @@ export default class Home extends Component {
                         </div>
                         <button className={s.onga}><AiOutlineArrowRight/></button>
                     </div>
-                    <div id={s.card1} className={s.card1}>
+                    <div id={s.card2} className={s.card1}>
                         <div className={s.as}>
-                            <h1>Marketing</h1><h2>250.000 <span>UZS</span></h2>
+                            <h1 className={s.hh3}>Biznes</h1><h2>250.000 <span>UZS</span></h2>
                         </div>
-                        <h3>Raqamli marketing bo'yicha <br /> to'liq kurs <span>| 48 soat</span></h3>
+                        <h3>Shaxsiy biznersni rivojlantin<br /> to'liq kurs <span>| 48 soat</span></h3>
                         <div className={s.yulduz}>
                         <AiFillStar className={s.yulduzcha}/><AiFillStar className={s.yulduzcha}/> <AiFillStar className={s.yulduzcha}/> <AiFillStar className={s.yulduzcha}/><AiFillStar className={s.yulduz1}/><p>4.1</p>
                         </div>
@@ -67,12 +81,144 @@ export default class Home extends Component {
                 </div>
                 
             </header>
+            <div className={s.divbig}>
+                <h1 className={s.divbigh1}>Bizning eng top kurslarimizni ko'rib chiqing</h1>
+                <div className={s.div1}>
+                    <div className={s.div1card1}>
+                        <img src={img1} alt="" />
+                        <div className={s.mm1}>
+                        <h2>Dasturlash</h2>
+                        </div>
+                        <h1>Java, Spring bo’yicha to’liq <br /> kurs.  (amaliy)</h1>
+                        <div className={s.yulduz}>
+                        <AiFillStar className={s.yulduzcha}/><AiFillStar className={s.yulduzcha}/> <AiFillStar className={s.yulduzcha}/> <AiFillStar className={s.yulduzcha}/><AiFillStar className={s.yulduz1}/><p>4.1</p> <span>(524)</span>
+                        </div>
+                        <div className={s.pdiv}>
+                        <p>Kurs hajmi</p><p>Kurs narxi</p>
+                        </div>
+                        <div className={s.h1div}>
+                            <h1>38 soat</h1><h1>250.000 <span>UZS</span></h1>
+                        </div>
+                        <button className={s.onga1}><AiOutlineArrowRight/></button>
+                    </div>
+                    <div className={s.div1card2}>
+                        <img src={img2} alt="" />
+                        <div className={s.mm2}>
+                        <h2>Dizayn</h2>
+                        </div>
+                        <h1>Illustrator boshlang’ichdan proffessionalgacha</h1>
+                        <div className={s.yulduz}>
+                        <AiFillStar className={s.yulduzcha}/><AiFillStar className={s.yulduzcha}/> <AiFillStar className={s.yulduzcha}/> <AiFillStar className={s.yulduzcha}/><AiFillStar className={s.yulduz1}/><p>4.1</p> <span>(524)</span>
+                        </div>
+                        <div className={s.pdiv}>
+                        <p>Kurs hajmi</p><p>Kurs narxi</p>
+                        </div>
+                        <div className={s.h1div}>
+                            <h1>38 soat</h1><h1>250.000 <span>UZS</span></h1>
+                        </div>
+                        <button className={s.onga1}><AiOutlineArrowRight/></button>
+                    </div>
+                </div>
+                <div className={s.div2}>
+                <div className={s.div1card3}>
+                        <img src={img3} alt="" />
+                        <div className={s.mm2}>
+                        <h2>Dizayn</h2>
+                        </div>
+                        <h1>Boshlang’ich Photoshop <br /> kurslari</h1>
+                        <div className={s.yulduz}>
+                        <AiFillStar className={s.yulduzcha}/><AiFillStar className={s.yulduzcha}/> <AiFillStar className={s.yulduzcha}/> <AiFillStar className={s.yulduzcha}/><AiFillStar className={s.yulduz1}/><p>4.0</p> <span>(524)</span>
+                        </div>
+                        <div className={s.pdiv}>
+                        <p>Kurs hajmi</p><p>Kurs narxi</p>
+                        </div>
+                        <div className={s.h1div}>
+                            <h2>42 soat</h2><h2>370.000 <span>UZS</span></h2>
+                        </div>
+                        <button className={s.onga1}><AiOutlineArrowRight/></button>
+                    </div>
+                    <div className={s.div1card4}>
+                        <img src={img4} alt="" />
+                        <div className={s.mm3}>
+                        <h2>Biznes</h2>
+                        </div>
+                        <h1>Shaxsiy biznesni rivojlan- <br /> tirish qoidalari</h1>
+                        <div className={s.yulduz}>
+                        <AiFillStar className={s.yulduzcha}/><AiFillStar className={s.yulduzcha}/> <AiFillStar className={s.yulduzcha}/> <AiFillStar className={s.yulduzcha}/><AiFillStar className={s.yulduz1}/><p>4.0</p> <span>(524)</span>
+                        </div>
+                        <div className={s.pdiv}>
+                        <p>Kurs hajmi</p><p>Kurs narxi</p>
+                        </div>
+                        <div className={s.h1div}>
+                            <h2>16 soat</h2><h2>420.000 <span>UZS</span></h2>
+                        </div>
+                        <button className={s.onga1}><AiOutlineArrowRight/></button>
+                    </div>
+                    <div className={s.div1card3}>
+                        <img src={img5} alt="" />
+                        <div className={s.mm4}>
+                        <h2>Dizayn</h2>
+                        </div>
+                        <h1>1C bo’yicha to’liq kurs <br />(amaliy)</h1>
+                        <div className={s.yulduz}>
+                        <AiFillStar className={s.yulduzcha}/><AiFillStar className={s.yulduzcha}/> <AiFillStar className={s.yulduzcha}/> <AiFillStar className={s.yulduzcha}/><AiFillStar className={s.yulduz1}/><p>4.1</p> <span>(524)</span>
+                        </div>
+                        <div className={s.pdiv}>
+                        <p>Kurs hajmi</p><p>Kurs narxi</p>
+                        </div>
+                        <div className={s.h1div}>
+                            <h2>36 soat</h2><h2>450.000 <span>UZS</span></h2>
+                        </div>
+                        <button className={s.onga1}><AiOutlineArrowRight/></button>
+                    </div>
+                </div>
+            </div>
+            <div className={s.divbig2}>
+            <div className={s.div12}>
+            <h1>Top kategoriyalar</h1><button>Barchasi</button>
+            </div>
+            <div className={s.divcards}>
+                <div className={s.carddiv}>
+                    <img src={img6} alt="" />
+                    <h2>Dasturlash</h2>
+                </div>
+                <div className={s.carddiv}>
+                    <img src={img7} alt="" />
+                    <h2>Dizayn</h2>
+                </div>
+                <div className={s.carddiv}>
+                    <img src={img8} alt="" />
+                    <h2> Biznes</h2>
+                </div>
+                <div className={s.carddiv}>
+                    <img src={img9} alt="" />
+                    <h2>Buxgalteriya</h2>
+                </div>
+                <div className={s.divcards2}>
+                <div className={s.carddiv}>
+                    <img src={img10} alt="" />
+                    <h2>Marketing</h2>
+                </div>
+                <div className={s.carddiv}>
+                    <img src={img11} alt="" />
+                    <h2>Menejment</h2>
+                </div>
+                <div className={s.carddiv}>
+                    <img src={img12} alt="" />
+                    <h2>Xavfsizlik</h2>
+                </div>
+                <div className={s.carddiv}>
+                    <img src={img13} alt="" />
+                    <h2>Salomatlik</h2>
+                </div>
+                </div>
+            </div>
+            </div>
 
 
 
 
-
-
+           
 
         <Footer/>
       </div>
--- pdp-strom/src/Pages/Js/NavbarMe.js
+++ pdp-strom/src/Pages/Js/NavbarMe.js
@@ -1,54 +1,39 @@
 import React, { Component } from 'react'
-        import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-export default class NavbarMe extends Component {
-  render() {
-    return (
-      <div>
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
-          </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../Css/Navbar.css';
+import Foto from '../img/Group.png';
 
-      </div>
-    )
-  }
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <div className="BOR">
+                <div className="navbar">
+                    <div className="logo_page">
+                        <img src={Foto} alt="" />
+                        <select id="cars">
+  <option value="volvo">Front End</option>
+  <option value="saab">Backend</option>
+  <option value="vw">MoDx</option>
+  <option value="Barcha kurslar" selected>Barcha kurslar</option>
+</select>
+
+                    </div>
+                    <div className="bat_page">
+                    <select id="cars1">
+  <option value="volvo">Rus tili</option>
+  <option value="saab">Ingli Tili</option>
+  <option value="vw">O'zbek Tili</option>
+  <option value="UZBEK" selected>UZBEK</option>
+</select>
+
+        <button>Kirish</button>
+                    </div>
+                </div>
+                </div>
+                
+            </div>
+        )
+    }
 }
